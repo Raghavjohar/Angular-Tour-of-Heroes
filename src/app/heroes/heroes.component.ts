32ec268f89231fdac4c2943b7eced4dfe95dead5
Raghav JOHAR <raghav.johar@amadeus.com>
2022-02-17 14:05:18 +0530

    First Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes : Hero[] = [];
+  // selectedHero?: Hero;
+  getHeroes(){
+    //this.heroes = this.heroService.getHeroes(); 
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
+
+  constructor(private heroService:HeroService, private messageService:MessageService) { }
+
+
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
+}
